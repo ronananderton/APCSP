81d8590de9238f8889db2ed9482086a3e03b3357
ronananderton <ronananderton@gmail.com>
2022-08-17 19:09:33 +0000

    day3

--- day2.py
+++ day2.py
@@ -1,5 +1,21 @@
-response = int(input("Pick a number 1 - 10\n"))
+number_chosen = input("Pick a number 1-10\n")
+num_siblings = input("How many siblings do you have?\n")
 
-print(type(response))
+if int(number_chosen) >= 6:
+    large_number = True
+else:
+    large_number = False
 
-if response 
+if int(num_siblings) >= 2:
+    sibling = True
+else:
+    sibling = False
+
+if large_number and sibling:
+    print("You will die in a house fire")
+elif not large_number and sibling:
+    print("You are going to lose your job")
+elif large_number and not sibling:
+    print("You will end up alone")
+else:
+    print("Have you ever heard of Krunker.io?")
