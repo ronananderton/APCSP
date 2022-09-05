c4202a31a8c93705f1f0b95fd03bef076ca9689e
ronananderton <ronananderton@gmail.com>
2022-09-05 02:50:19 +0000

    1-3 done

---
+++ 2_Control_Flow.py
@@ -0,0 +1,21 @@
+number_chosen = input("Pick a number 1-100\n")
+num_siblings = input("How many siblings do you have?\n")
+
+if int(number_chosen) >= 50:
+    large_number = True
+else:
+    large_number = False
+
+if int(num_siblings) >= 2:
+    sibling = True
+else:
+    sibling = False
+
+if large_number and sibling:
+    print("You will die in a house fire")
+elif not large_number and sibling:
+    print("You are going to lose your job")
+elif large_number and not sibling:
+    print("You will end up alone")
+else:
+    print("You should invest in NFTs")
