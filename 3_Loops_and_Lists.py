fec09ba4868b3638dcc790431730bf7c93f622bd
ronananderton <ronananderton@gmail.com>
2022-09-05 03:23:42 +0000

    1-4 done

--- 3_Loops_and_Lists.py
+++ 3_Loops_and_Lists.py
@@ -1,5 +1,7 @@
 hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
+
 prices = [30, 25, 40, 20, 20 ,35, 50, 35]
+
 last_week = [2, 3, 5, 8, 4, 4, 6, 2]
 
 total_price = 0
---
+++ 4_Functions.py
@@ -0,0 +1,26 @@
+def f_to_c(f):
+    return (float(f) - 32) * 5/9
+
+def c_to_f(c):
+    return float(c) * 9/5 + 32
+
+def c_to_k(c):
+    return float(c) + 273.15
+
+def k_to_c(k):
+    return float(k) - 273.15
+
+input_type = input("Is your temperature measured in celcius, fahrenheit, or kelvin?\n" )
+temp = input("What is the temperature?\n")
+output_type = input("Would you like to convert to celcius, fahrenheit, or kelvin?\n")
+
+if input_type == "celcius" and output_type == "fahrenheit":
+    print(f"Your new temperature is {c_to_f(temp)} degrees fahrenheit.")
+elif input_type == "fahrenheit" and output_type == "celcius":
+    print(f"Your new temperature is {f_to_c(temp)} degrees celcius.")
+elif input_type == "celcius" and output_type == "kelvin":
+    print(f"Your new temperature is {c_to_k(temp)} degrees kelvin.")
+elif input_type == "kelvin" and output_type == "celcius":
+    print(f"Your new temperature is {k_to_c(temp)} degrees celcius.")
+else:
+    print("The requested converter is currently unavailable :(")
