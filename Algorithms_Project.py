fb4da26616bf99d8b7910b84990195f8fa5f7a71
ronananderton <ronananderton@gmail.com>
2022-10-11 06:08:56 +0000

    Done

--- Algorithms_Project.py
+++ Algorithms_Project.py
@@ -7,51 +7,42 @@ import datetime
 data = requests.get("https://sheetdb.io/api/v1/nbalaz9v6jo2q").json()
 
 today = [datetime.datetime.now().strftime("%x")]
-today.append(int(input("How many minutes do you have to do homework today? \n")))
+today.append(int(input("How many minutes do you have to do homework today?\n")))
 
 tomorrow = [(datetime.datetime.now() + datetime.timedelta(days=1)).strftime("%x")]
-tomorrow.append(int(input("How many minutes do you have to do homework tomorrow? /n")))
+tomorrow.append(int(input("How many minutes do you have to do homework tomorrow?\n")))
 
 dayafter = [(datetime.datetime.now() + datetime.timedelta(days=2)).strftime("%x")]
-dayafter.append(int(input("How many minutes do you have to do homework the day after? /n")))
-
+dayafter.append(int(input("How many minutes do you have to do homework the day after?\n")))
 
 # ASSIGNMENT STARTS HERE
 
-# FIRST WRITE A COMMENT DESCRIBING YOUR ALGORITHM
-# HOW WILL YOUR ALGORITHM WORK? WHAT IS THE HEURISTIC YOU ARE USING? WHAT HAPPENS WHEN YOU HAVE TOO MUCH HOMEWORK AND NOT ENOUGH MINUTES? WHAT HAPPENS WHEN YOU HAVE ENOUGH MINUTES TO COMPLETE AN ASSIGNMENT HALFWAY, BUT NOT COMPLETELY? WALK ME THROUGH AN EXAMPLE WITH A FEW ASSIGNMENTS.  
-
-#The goal of the algorithm is to find the most eficient way to complete all homework assignments on time. 
-# The algorithm will first prioritize the assignments due the earliest. If multiple assignments are due the 
-# same day, the priority value of the assignments will then dictate. This will sort the the assignments by 
-# importance and the succession of assignments will be assigned based on the first day there is enough time 
-# to complete it. The point value of the assignments is relative only to its individual class and will not 
-# be taken into consideration. If there is not enough time to complete an assignment any of the three days, 
-# the program will suggest to get the work from someone. 
-
+# FIRST WRITE A COMMENT DESCRIBING YOUR ALGORITHM. HOW WILL YOUR ALGORITHM WORK? WHAT IS THE HEURISTIC YOU 
+# ARE USING? WHAT HAPPENS WHEN YOU HAVE TOO MUCH HOMEWORK AND NOT ENOUGH MINUTES? WHAT HAPPENS WHEN YOU HAVE 
+# ENOUGH MINUTES TO COMPLETE AN ASSIGNMENT HALFWAY, BUT NOT COMPLETELY? WALK ME THROUGH AN EXAMPLE WITH A 
+# FEW ASSIGNMENTS.  
+# The goal of the algorithm is to prioritize my worktime to complete the most important assignments first.
+# The algorithm will recognize the priority value of the assignments which will dictate order. 
 # NEXT WRITE YOUR ALGORITHM OUT IN PSEUDOCODE
-#
-# The program will suggest the user with a schedule of when to do which homework asignments.
-#
-# Sort data by "Due Date" and "Priority"
-# Create dictionaries for available time each day and the assignements given.
-
 
 #FINALLY - IMPLEMENT YOUR ALGORITHM! IF YOU FINISH, THINK ABOUT HOW TO OPTIMIZE YOUR ALGORITHM! MAYBE ADD A "I DONT WANT TO" OPTION TO PUSH YOUR HOMEWORK TO THE NEXT DAY OR A "STAY UP LATE" TO FINISH A PRIORITY ASSIGNMENT
 
+new_data = sorted(data, key = lambda x: int(x["Priority"]))[::-1]
 
-def schedule(input_data):
-    time_inputs = int(input(day))
-    day_totals = {"today" : x , "tomorrow" : x , "dayafter" : x}
-    assignments = {"today" : [], "tomorrow" : [] , "dayafter" : []}
-    assignment_unassigned = []
+def schedule(assignment_list):
+    day_total = {"today": 0, "tomorrow": 0, "dayafter": 0}
+    day_assignments = {"today": [], "tomorrow": [], "dayafter": []}
+    assignments_scheduled = []
 
-    for i in range(len(input_data)):
-        
-        i = len(assignment_unassigned)
-        assignment = input_data[i]
+    for day in day_total.keys():
+        for assignment in assignment_list:
+            required_time = int(assignment['Required Time'])
 
-        if day_totals[day] + int(assignment["Required Time"]) <= time_inputs(day):
-            assignment_unassigned.append(assignment)
-            day_totals += int(assignment["Required Time"])
-            input_data.remove(assignment)
+            if day_total[day] + required_time <= globals()[day][1] and assignment not in assignments_scheduled:
+                day_total[day] += required_time
+                day_assignments[day].append(assignment['Assignment'])
+                assignments_scheduled.append(assignment)
+    return day_assignments
+
+
+print(schedule(new_data))
