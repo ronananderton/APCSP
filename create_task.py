6850937e0554e0902de08e6a111c06d9c6c6c8ea
ronananderton <ronananderton@gmail.com>
2023-02-27 20:54:51 +0000

    r

---
+++ create_task.py
@@ -0,0 +1,11 @@
+import random
+rolls = ["1" , "2" , "3" , "4" , "5" , "6"]
+
+point_values = [ -200 , -200 , -200 , 200 , 200 , 200 ]
+
+total_points == 500
+
+die_choice = "You have 500 points. There are two die in front of you. (answer 1 / 2)"
+
+if die_choice == "1":
+    randran(1 <= X <= 6)
