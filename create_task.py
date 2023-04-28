c5e56da99b64e96db7342bc6eeb1f5c50e048bf9
ronananderton <ronananderton@gmail.com>
2023-04-28 18:23:48 +0000

    t

--- create_task.py
+++ create_task.py
@@ -1,17 +1,18 @@
 import random
 
-die_2_rolls = ["Double Points" , "Gain A Roll" , "Gain 500 Points" , "Lose All Points" ]
+die_2_rolls = ["Double Points" , "Gain A Roll" , "Gain 500 Points" , "Lose All Points"]
 r = 5
 
 def run_dice_game(r):
     total_points = 500
     
     while r > 0 and total_points >= 0 and total_points < 1200:
-        die_choice = input(f'''You have {total_points} points.  You have {r} roll(s) left. Choose your die. (answer 1 or 2) \n''')
+        die_choice = input(f'''You have {total_points} points.  You have {r} roll(s) 
+        left. Choose your die. (answer 1 or 2) \n''')
 
         if die_choice == "1":
             print("You chose the first die.")
-            r1 = random.randint(0, 6)
+            r1 = random.randint(1, 6)
             if r1 <= 3:
                 total_points -= 200
                 print(f"You rolled a {r1}. You lost 200 points.")
@@ -42,7 +43,7 @@ def run_dice_game(r):
     return total_points
 
 print('''Welcome to the dice game! There are two dice in front of you. The first 
-is a basic 6-sided die. If you roll a 1, 2, or a 3, you will lose 200 points. If you roll a 4, 5, or a 6, 
+is a basic 6-sided die. If you roll a 1, 2, or 3, you will lose 200 points. If you roll a 4, 5, or a 6, 
 you will gain 200 points. The second is a special four-sided die. This die will result in doublng your 
 points, gaining a roll, gaining 500 points, or losing all of your points. Your goal is to obtain 1,200 points. 
 You lose if your points go below 0 or if you did not reach the goal. ''')
